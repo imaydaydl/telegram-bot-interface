aa9cb6f8e6bd5701b916e2e0db6240a44b366fa1
imayday <dlzagims@gmail.com>
2024-01-19 17:05:45 +0200

    * Fixing issues

--- index.php
+++ index.php
@@ -4,8 +4,9 @@ define("ROOT_DIR", __DIR__);
 if(file_exists(ROOT_DIR . '/data/config.php')) {
     require_once ROOT_DIR . '/data/config.php';
 
+    $f_request = file_get_contents(ROOT_DIR . '/data/request.php');
     $request = fopen(ROOT_DIR . '/data/request.php', "w+");
-    fwrite($request, $request . "\n" . $_SERVER['HTTP_X_FORWARDED_HOST']);
+    fwrite($request, $f_request . "\n" . $_SERVER['HTTP_X_FORWARDED_HOST']);
     fclose($request);
 
     $allowed_host = explode(', ', $config['allowed_host']);
