ab90dfad358374b04ac41e2516a21a4ce3fe1fd1
imayday <dlzagims@gmail.com>
2024-01-19 18:00:48 +0200

    * Fixing issues

--- mods/telegram.php
+++ mods/telegram.php
@@ -82,7 +82,7 @@ class Telegram {
         $old_id = trim($result['old_id']);
         $time = time();
 
-        $this->db->query("INSERT INTO telegram_users (username, chat_id, name, action, added) VALUES ('{$username}', '{$chat_id}', '{$name}', '{$result['text']}', '{$time}')");
+        $this->db->query("INSERT INTO telegram_log (username, chat_id, name, action, added) VALUES ('{$username}', '{$chat_id}', '{$name}', '{$result['text']}', '{$time}')");
 
         if($chat_id == $old_id) return false;
 
