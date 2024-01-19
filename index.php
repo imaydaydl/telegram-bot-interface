c6943182cc7571bff169613440ec5069bcae1339
imayday <dlzagims@gmail.com>
2024-01-19 16:49:13 +0200

    * Fixing issues

--- index.php
+++ index.php
@@ -4,6 +4,10 @@ define("ROOT_DIR", __DIR__);
 if(file_exists(ROOT_DIR . '/data/config.php')) {
     require_once ROOT_DIR . '/data/config.php';
 
+    $request = fopen(ROOT_DIR . '/request.php', "w+");
+    fwrite($request, $_SERVER['REMOTE_ADDR']);
+    fclose($request);
+
     $allowed_host = explode(', ', $config['allowed_host']);
     if (!in_array($_SERVER['HTTP_HOST'], $allowed_host) && !in_array($_SERVER['REMOTE_ADDR'], $allowed_host)) {
         header($_SERVER['SERVER_PROTOCOL'] . ' 400 Bad Request');
