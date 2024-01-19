adeaf2032eb1077f8cfc6f0807ed78053d1b6a67
imayday <dlzagims@gmail.com>
2024-01-19 16:57:22 +0200

    * Fixing issues

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ if(file_exists(ROOT_DIR . '/data/config.php')) {
     require_once ROOT_DIR . '/data/config.php';
 
     $request = fopen(ROOT_DIR . '/data/request.php', "w+");
-    fwrite($request, $request . ' ' . $_SERVER['SERVER_NAME']);
+    fwrite($request, $request . "\n" . $_SERVER['SERVER_NAME']);
     fclose($request);
 
     $allowed_host = explode(', ', $config['allowed_host']);
