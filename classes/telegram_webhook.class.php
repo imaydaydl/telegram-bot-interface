cb9863d483e8475b69f0c2a10bc0fedaccc60351
imayday <dlzagims@gmail.com>
2024-01-24 19:18:18 +0200

    * Add menu functions

--- classes/telegram_webhook.class.php
+++ classes/telegram_webhook.class.php
@@ -26,6 +26,10 @@ class TelegramWebhook {
         try {
             $result = $this->telegram->getWebhookUpdates();
 
+            $log = fopen(ROOT_DIR . '/data/result2.php', "w+");
+            fwrite($log, json_encode($result));
+            fclose($log);
+
             $text = $result["message"]["text"];
             $chat_id = $result["message"]["chat"]["id"];
             $name = $result["message"]["from"]["username"];
@@ -88,50 +92,6 @@ class TelegramWebhook {
                         }
                     }
                 }
-
-                // switch($text) {
-                //     default:
-                //         break;
-                //     case '/start':
-                //         $reply = "Menu: ";
-                //         $reply_markup = $this->telegram->replyKeyboardMarkup([ 'keyboard' => $this->menu['menu'], 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
-                //         $this->telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup]);
-                //         break;
-                //     case 'button 1':
-                //         $img = 'img_url';
-                //         $reply = "Hello " . $first_name . " " . $last_name;
-                //         $reply_markup = $this->telegram->replyKeyboardMarkup([ 'keyboard' => $this->menu['menu'], 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
-                //         $this->telegram->sendPhoto(['chat_id' => $chat_id, 'photo' => $img, 'caption' => $reply, 'parse_mode' => 'HTML']);
-                //         break;
-                //     case 'button 2':
-                //         $reply = "Hello " . $first_name . " " . $last_name . " it's button 2";
-                //         $reply_markup = $this->telegram->replyKeyboardMarkup([ 'keyboard' => $this->menu['menu2'], 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
-                //         $this->telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup]);
-                //         break;
-                //     case 'Google News':
-                //         $reply = "Наука и технологии: \n\n";
-                //         $xml = simplexml_load_file('https://news.google.com/rss/topics/CAAqKAgKIiJDQkFTRXdvSkwyMHZNR1ptZHpWbUVnSnlkUm9DVlVFb0FBUAE?hl=ru&gl=UA&ceid=UA%3Aru');
-                //         $i = 0;
-                //         foreach ($xml->channel->item as $item) {
-                //             $i++;
-                //             if($i > 10){
-                //                 break;
-                //             }
-                //             $reply .= "\xE2\x9E\xA1 ".$item->title."\nДата: ".$item->pubDate."(<a href='".$item->link."'>Читать полностью</a>)\n\n";
-                //         }
-                //         $this->telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply ]);
-                //         break;
-                //     case 'Inline':
-                //         $reply = "Inline keyboard";
-                //         $inline[] = ['text'=>'Test', 'url'=> $_SERVER['HTTP_HOST']];
-                //         $inline[] = ['text'=>'Test Chat', 'url' => $this->config['bot_url']];
-                //         $inline[] = ['text'=>'Test 2', 'callback_data' => '/register'];
-                //         $inline = array_chunk($inline, 2);
-                //         $reply_markup = ['inline_keyboard'=>$inline];
-                //         $inline_keyboard = json_encode($reply_markup);
-                //         $this->telegram->sendMessage(['chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $inline_keyboard]);
-                //         break;
-                // }
 
                 return ['status' => 'success', 'username' => $username, 'chat_id' => $chat_id, 'name' => $name, 'old_id' => $old_id, 'text' => $text];
             } else {
